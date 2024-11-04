9e141b19e3e987269a5b76c6e6a7b76152184c38
Kristian Botnen <kristian.botnen@uib.no>
2024-11-04 07:18:14 +0100

    Dicethrow and random module import

--- src/b3dtools/b3d.py
+++ src/b3dtools/b3d.py
@@ -1,4 +1,13 @@
-
+import random
 
 def b3d_print(message):
     print(message)
+
+def b3d_dicethrow(throws):
+    result_array = []
+    
+    for i in range(0, throws):
+        dice_throws = random.randrange(1,7)
+        result_array.append(dice_throws)
+
+    return result_array
